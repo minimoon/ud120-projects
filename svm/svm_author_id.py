88d6cc3b49210e1db1912527bc789f5791c2622b
Unknown <sasivimoon@gmail.com>
2017-05-23 14:43:10 +0700

    add svm code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,36 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+t0 = time()
+from sklearn.svm import SVC
+xt = features_train
+yt = labels_train
+xs = features_test
+ys = labels_test
+#for i in xt[0:10]:
+    #print i
+from sklearn.naive_bayes import GaussianNB
+clf = SVC(kernel='linear')
+clf.fit(xt, yt)
+#SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+    #max_iter=-1, probability=False, random_state=None, shrinking=True,
+    #tol=0.001, verbose=False)
+pred = clf.predict(xs)
+
+from sklearn.metrics import accuracy_score
+score = accuracy_score(pred,ys)
+print score
+
+#print total time
+print "Total  time(s):", round(time()-t0, 3), "s"
+print "Total  time(m):", round(time()-t0, 3)/60, "m"
 
 #########################################################
-
-
+'''
+no. of Chris training emails: 7936
+no. of Sara training emails: 7884
+0.984072810011
+Total  time(s): 161.095 s
+Total  time(m): 2.68491666667 m
+'''
